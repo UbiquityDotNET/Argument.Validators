aecb5edec758323125a01545d70876c3dc955bd7
smaillet <smaillet@users.noreply.github.com>
2018-10-08 00:18:37 -0700

    updated push-docs to test for correct git remote URL with the actual docs folder, otherwise it gets the settings from the parent repo which isn't always the correct URL (Azure Pipelines builds don't use the same checkout strategy as AppVeyor does)

--- Push-Docs.ps1
+++ Push-Docs.ps1
@@ -2,27 +2,26 @@ if($env:APPVEYOR_PULL_REQUEST_NUMBER)
 {
     return;
 }
-
-# Docs must only be updated from a build with the official repository as the default remote.
-# This ensures that the links to source in the generated docs will have the correct URLs
-# (e.g. docs pushed to the official repository MUST not have links to source in some private fork)
-$remoteUrl = git ls-remote --get-url
-if($remoteUrl -ne "https://github.com/UbiquityDotNET/Argument.Validators.git")
-{
-    throw "Pushing docs is only allowed when the origin remote is the official source release. Current remote: $remoteUrl"
-}
-
-if($env:APPVEYOR)
-{
-    git config --global credential.helper store
-    Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:docspush_access_token):x-oauth-basic@github.com`n"
-    git config --global user.email "$env:docspush_email"
-    git config --global user.name "$env:docspush_username"
-}
-
 pushd .\BuildOutput\docs -ErrorAction Stop
 try
 {
+    # Docs must only be updated from a build with the official repository as the default remote.
+    # This ensures that the links to source in the generated docs will have the correct URLs
+    # (e.g. docs pushed to the official repository MUST not have links to source in some private fork)
+    $remoteUrl = git ls-remote --get-url
+    if($remoteUrl -ne "https://github.com/UbiquityDotNET/Argument.Validators")
+    {
+        throw "Pushing docs is only allowed when the origin remote is the official source release. Current remote: $remoteUrl"
+    }
+
+    if($env:APPVEYOR)
+    {
+        git config --global credential.helper store
+        Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:docspush_access_token):x-oauth-basic@github.com`n"
+        git config --global user.email "$env:docspush_email"
+        git config --global user.name "$env:docspush_username"
+    }
+
     Write-Information "Adding files to git"
     git add * | Out-File -Append docs-commit.log
 
